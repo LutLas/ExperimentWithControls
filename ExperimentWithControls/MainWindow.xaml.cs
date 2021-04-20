22b46ef3b3b62a8bf3f140204173a22f5d2c2fe3
Lutaka Chihota <lutakachihota@gmail.com>
2021-04-20 20:20:09 +0200

    LastCommit

--- ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/MainWindow.xaml.cs
@@ -23,6 +23,61 @@ namespace ExperimentWithControls
         public MainWindow()
         {
             InitializeComponent();
+            numberTextBox.Text = "";
+            number.Text = "#";
+        }
+
+        private void NumberTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            number.Text = numberTextBox.Text;
+        }
+
+        private void NumberTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void SmallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void ReadOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            
+            if (readOnlyComboBox.SelectedItem is ComboBoxItem comboBoxItem)
+            {
+                number.Text = comboBoxItem.Content.ToString();
+            }
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (editableComboBox.SelectedItem is ComboBoxItem comboBoxItem)
+            {
+                number.Text = comboBoxItem.Content.ToString();
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = ridiculousSlider.Value.ToString("000-000-000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void ListBoxItem_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
         }
     }
 }
